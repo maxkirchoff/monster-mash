7c6dca48b15b5266d3e9f17caf50bc70c92bddc5
Max Kirchoff <max@shopigniter.com>
2012-10-22 22:28:51 -0700

    THE MONSTER MASH!

---
+++ bug-monster.py
@@ -0,0 +1,44 @@
+import socket, ssl, subprocess, time, os, json, random, httplib, urllib, ConfigParser
+from subprocess import *
+
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+
+server = Config.get("irc", "server")
+port = Config.getint("irc", "port")
+channel = Config.get("irc", "channel")
+botnick = Config.get("bugs", "nick")
+password = Config.get("irc", "password")
+
+def ping(): 
+  ircsock.send("PONG :pingis\n")  
+
+def sendmsg(msg): 
+  ircsock.send("PRIVMSG "+ channel +" :"+ msg +"\n")
+
+def voice(ircstr):
+  (_, _, voicestr) = ircstr.partition("!v")
+  voicestr = voicestr.upper()
+  sendmsg(voicestr)
+  
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server, port)) 
+ircsock = ssl.wrap_socket(s)
+ircsock.send("PASS "+ password +"\n") 
+ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +"QA PUSHER BOT!!!! \n") 
+ircsock.send("NICK "+ botnick +"\n") 
+ircsock.send("JOIN "+ channel +"\n")
+
+while 1: 
+  ircmsg = ircsock.recv(2048) 
+  ircmsg = ircmsg.strip('\n\r') 
+  print(ircmsg)
+   
+  if ircmsg.find(":!v") != -1:
+    voice(ircmsg)
+
+  if ircmsg.find("bug") != -1:
+    sendmsg("BUGS?!?!?! I LOVE BUGS!")
+
+  if ircmsg.find("PING :") != -1: 
+    ping()
